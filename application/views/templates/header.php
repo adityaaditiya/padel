7c6211f265e5288484dad28cef4d46de9f4996a6
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-08-25 15:23:30 +0700

    feat: allow cashiers to access finance menu

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -17,7 +17,18 @@
         <ul class="navbar-nav mr-auto">
             <?php if ($this->session->userdata('logged_in')): ?>
                 <?php $role = $this->session->userdata('role'); ?>
-                <li class="nav-item"><a class="nav-link" href="<?php echo site_url('booking'); ?>">Booking</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="bookingDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Booking</a>
+                    <div class="dropdown-menu" aria-labelledby="bookingDropdown">
+                        <?php if ($role === 'pelanggan'): ?>
+                            <a class="dropdown-item" href="<?php echo site_url('booking/my'); ?>">Booking Saya</a>
+                        <?php endif; ?>
+                        <a class="dropdown-item" href="<?php echo site_url('booking'); ?>">Jadwal Booking Lapangan</a>
+                        <?php if (in_array($role, ['kasir','admin_keuangan','owner'])): ?>
+                            <a class="dropdown-item" href="<?php echo site_url('booking/cancelled'); ?>">Booking Batal</a>
+                        <?php endif; ?>
+                    </div>
+                </li>
                 <?php if (in_array($role, ['kasir','admin_keuangan','owner'])): ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo site_url('pos'); ?>">POS</a></li>
                 <?php endif; ?>
@@ -25,7 +36,7 @@
                     <li class="nav-item"><a class="nav-link" href="<?php echo site_url('courts'); ?>">Lapangan</a></li>
                     <li class="nav-item"><a class="nav-link" href="<?php echo site_url('reports'); ?>">Laporan</a></li>
                 <?php endif; ?>
-                <?php if (in_array($role, ['admin_keuangan','owner'])): ?>
+                <?php if (in_array($role, ['kasir','admin_keuangan','owner'])): ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo site_url('finance'); ?>">Keuangan</a></li>
                 <?php endif; ?>
                 <?php if (in_array($role, ['kasir','admin_keuangan','owner'])): ?>
