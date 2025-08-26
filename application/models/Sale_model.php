d78c20e34dae496e63310ce55bc1bc70fe1a743b
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-08-26 20:13:50 +0700

    Allow date range filtering on transactions

--- application/models/Sale_model.php
+++ application/models/Sale_model.php
@@ -10,7 +10,14 @@ class Sale_model extends CI_Model
 
     public function insert($data)
     {
-        $this->db->insert($this->table, $data);
+        $insertData = [
+            'id_kasir'      => $data['id_kasir'],
+            'customer_id'   => isset($data['customer_id']) ? $data['customer_id'] : null,
+            'nomor_nota'    => $data['nomor_nota'],
+            'total_belanja' => $data['total_belanja']
+        ];
+
+        $this->db->insert($this->table, $insertData);
         return $this->db->insert_id();
     }
 
@@ -19,11 +26,17 @@ class Sale_model extends CI_Model
         return $this->db->get_where($this->table, ['id' => $id])->row();
     }
 
-    public function get_all()
+    public function get_all($start_date = null, $end_date = null)
     {
         $this->db->select('s.*, u.nama_lengkap AS customer_name');
         $this->db->from($this->table . ' s');
         $this->db->join('users u', 'u.id = s.customer_id', 'left');
+        if ($start_date) {
+            $this->db->where('DATE(s.tanggal_transaksi) >=', $start_date);
+        }
+        if ($end_date) {
+            $this->db->where('DATE(s.tanggal_transaksi) <=', $end_date);
+        }
         $this->db->order_by('s.tanggal_transaksi', 'DESC');
         return $this->db->get()->result();
     }
